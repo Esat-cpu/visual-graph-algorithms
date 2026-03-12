21f3b63feb13d0ebaf3ff8e5b2671ba6db35db6e
Esat <saygina7@gmail.com>
2026-03-13 01:07:54 +0300

    test: add bellman ford test

---
+++ tests/bellman_ford_test.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const { Graph } = require('../js/graph.js');
+const { bellman_ford } = require('../js/bellman_ford.js');
+const assert = require('assert');
+
+
+function test_1() {
+    const graph = new Graph();
+
+    graph.addNode(0, 0);
+    graph.addNode(1, 1);
+    graph.addNode(2, 2);
+    graph.addNode(3, 3);
+
+    graph.addEdge(0, 1, 4);
+    graph.addEdge(0, 2, 2);
+    graph.addEdge(2, 1, 1);
+    graph.addEdge(2, 3, 5);
+    graph.addEdge(1, 3, 1);
+
+    const steps = bellman_ford(graph, 0).steps;
+    const lastDistList = steps[steps.length - 1].distance;
+
+    assert(lastDistList[0] === 0, "Start Node");
+    assert(lastDistList[1] === 3, "Second");
+    assert(lastDistList[2] === 2, "Third");
+    assert(lastDistList[3] === 4, "Fourth");
+    console.log("Bellman Ford Test 1 Succesful!");
+}
+
+
+test_1();
